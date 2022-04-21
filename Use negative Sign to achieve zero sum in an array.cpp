3099b17159b94e2cf6f94c9b09d06fdf2b70ca8a
Wishhhhh <86937408+Wu-yu-shiun@users.noreply.github.com>
2022-04-21 13:55:18 +0800

    Create Use negative Sign to achieve zero sum in an array.cpp

---
+++ Use negative Sign to achieve zero sum in an array.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+bool check(int* nums,int numsSize,int target){
+    printf("now length: %d\n",numsSize);
+    bool dp[numsSize+1][target+1];
+    for(int i=0;i<numsSize+1;++i){
+        for(int j=0;j<target+1;++j){
+            if(j==0) dp[i][j]=true;
+            else if(i==0) dp[i][j]=false;
+            else dp[i][j]=(j<nums[i-1])?dp[i-1][j]:(dp[i-1][j]||dp[i-1][j-nums[i-1]]);
+        }
+    }
+    return dp[numsSize][target];
+}
+
+void revise(int *nums,int numsSize){
+    nums=(int*)realloc(nums, sizeof(int)*(numsSize+1));
+    numsSize++;
+    nums[numsSize-1]=1;
+}
+
+int main(){
+
+    //input
+    int numsSize;
+    cout<<"size: ";
+    cin>>numsSize;
+    int* nums=(int*)malloc(sizeof(int)*numsSize);
+    for(int i=0;i<numsSize;++i){
+        cin>>nums[i];
+    }
+    
+    int result=0;
+    while(true){
+        
+        int sum=0,target;
+        for(int i=0;i<numsSize;++i){
+            sum+=nums[i];
+        }
+        target=sum/2;
+        
+        if(sum%2!=0){
+            revise(nums,numsSize);
+            numsSize++;
+            result++;
+            continue;
+        }
+        
+        if(!check(nums,numsSize,target)){
+            revise(nums,numsSize);
+            numsSize++;
+            result++;
+            continue;
+        }
+
+        break;
+    }
+    
+    printf("Ans: %d\n",result);
+    return 0;
+}
